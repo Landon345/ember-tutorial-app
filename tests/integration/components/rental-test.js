e829fd0550e5a8f43ca16a89434c78103049f02b
Landon Schlangen <landonschlangen@Landons-MacBook-Pro.local>
2020-09-16 12:09:30 -0500

    ember tutorial complete

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -5,6 +5,9 @@ import { hbs } from "ember-cli-htmlbars";
 
 module("Integration | Component | rental", function (hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.owner.setupRouter();
+  });
 
   test("it renders information about a rental property", async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
@@ -12,6 +15,7 @@ module("Integration | Component | rental", function (hooks) {
 
     this.setProperties({
       rental: {
+        id: "grand-old-mansion",
         title: "Grand Old Mansion",
         owner: "Veruca Salt",
         city: "San Francisco",
@@ -33,6 +37,9 @@ module("Integration | Component | rental", function (hooks) {
 
     assert.dom("article").hasClass("rental");
     assert.dom("article h3").hasText("Grand Old Mansion");
+    assert
+      .dom("article h3 a")
+      .hasAttribute("href", "/rentals/grand-old-mansion");
     assert.dom("article .detail.owner").includesText("Veruca Salt");
     assert.dom("article .detail.type").includesText("Standalone");
     assert.dom("article .detail.location").includesText("San Francisco");
